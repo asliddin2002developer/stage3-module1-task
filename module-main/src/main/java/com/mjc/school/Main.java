3543c57d9b42aaeba557dbf3d3a5542d5cc873a8
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-03-03 08:47:16 +0500

    clear main

--- module-main/src/main/java/com/mjc/school/Main.java
+++ module-main/src/main/java/com/mjc/school/Main.java
@@ -8,8 +8,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        DataSource database = DataSource.getInstance();
-        database.readFile("content.txt");
 
     }
 }
