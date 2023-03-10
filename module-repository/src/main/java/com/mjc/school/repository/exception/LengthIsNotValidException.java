3be401ac1fbbb9fc5f308358f32a774834c9baba
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-03-10 15:25:05 +0500

    code clean up 2

--- module-repository/src/main/java/com/mjc/school/repository/exception/LengthIsNotValidException.java
+++
@@ -1,7 +0,0 @@
-package com.mjc.school.repository.exception;
-
-public class LengthIsNotValidException extends Exception{
-    public LengthIsNotValidException(String msg){
-        super(msg);
-    }
-}
